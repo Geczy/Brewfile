8cfa97ba87767f633b64cbbfba806292199fa0f8
Geczy <Geczy@users.noreply.github.com>
2023-05-05 12:35:54 -0500

    Backup Brewfile 2023-05-05 12:35:54

--- backup-brew.sh
+++ backup-brew.sh
@@ -9,6 +9,7 @@ cd "$repository"
 rm Brewfile || true
 brew bundle dump -f
 npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' >npm-global-list
+yarn global list --depth=0 | sed '1d' | awk '{gsub(/@.*$/,"",$1); print}' >yarn-global-list
 git add .
 git commit -m "Backup Brewfile $(date "+%Y-%m-%d %H:%M:%S")"
 git push origin master
